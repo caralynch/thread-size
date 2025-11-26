99fa099a69e754c91aef42becf9ed87a5aa416bf
cara <cara.p.lynch@gmail.com>
2025-11-26 16:04:14 +0000

    typo fix

--- Scripts/1_submit_thread_start_jobscripts.sh
+++ Scripts/1_submit_thread_start_jobscripts.sh
@@ -3,7 +3,7 @@
 # List of subreddits to process
 SUBREDDITS=("conspiracy" "crypto" "politics")
 
-JOBSCRIPT="/home/ucabcpl/Scratch/thread_size/thread-size/Scripts/1_run_thread_size_jobscript.sh"
+JOBSCRIPT="/home/ucabcpl/Scratch/thread_size/thread-size/Scripts/1_run_thread_start_jobscript.sh"
 
 # Make sure the jobscript exists
 if [ ! -f "$JOBSCRIPT" ]; then
