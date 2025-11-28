b876e924853fef8e9594e95f0a49ba5347430c77
cara <cara.p.lynch@gmail.com>
2025-11-28 14:46:12 +0000

    modified jobscript path

--- Scripts/Jobscripts/1_submit_jobscripts.sh
+++ Scripts/Jobscripts/1_submit_jobscripts.sh
@@ -3,7 +3,7 @@
 # List of subreddits to process
 SUBREDDITS=("conspiracy" "crypto" "politics")
 
-JOBSCRIPT="1_run_thread_start_jobscript.sh"
+JOBSCRIPT="./1_run_thread_start_jobscript.sh"
 
 # Make sure the jobscript exists
 if [ ! -f "$JOBSCRIPT" ]; then
